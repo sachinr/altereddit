8317168f86d9d668275bf87d59b71b1ae7d12c71
Sachin Ranchod <sachin.ranchod@gmail.com>
2012-10-01 14:18:46 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altereddit::Application.config.secret_token = 'c4be63cbd34c3b4d9c151bf097aaffa6df9fcdd2c38b160ecd3577ddc1cf0521523e729e50dab85090913066b810172b0bb17edd4f3c1ee0dfd20a53acc3b485'
